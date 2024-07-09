bef603a68928067997e709b3fe504ec978a00501
猪猪Seki <142310716+zzseki@users.noreply.github.com>
2024-07-09 10:47:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -28,10 +28,13 @@ class WeatherPlugin(BasePlugin):
     @handler(NormalMessageResponded)
     async def normal_message_responded(self, ctx: EventContext, **kwargs):
         response_text = ctx.event.response_text
-        #今天|明天|后天|大后天|一天后|两天后|三天后|四天后|五天后|1天后|2天后|3天后|4天后|5天后|6天后
+
+        #今天|明天|后天|大后天|一天后|两天后|三天后|四天后|五天后|六天后|1天后|2天后|3天后|4天后|5天后|6天后|
+        # 一天之后|两天之后|三天之后|四天之后|五天之后|六天之后|1天之后|2天之后|3天之后|4天之后|5天之后|6天之后|
+        # 一天以后|两天以后|三天以后|四天以后|五天以后|六天以后|1天以后|2天以后|3天以后|4天以后|5天以后|6天以后
 
         # 定义正则表达式模式，匹配形如 城市:xxx 的字符串
-        CITY_PATTERN = re.compile(r"城市:(.*?)(今天|明天|后天|大后天|一天后|两天后|三天后|四天后|五天后|六天后|1天后|2天后|3天后|4天后|5天后|6天后)的天气情况")
+        CITY_PATTERN = re.compile(r"城市:(.*?)(今天|明天|后天|大后天|一天后|两天后|三天后|四天后|五天后|六天后|1天后|2天后|3天后|4天后|5天后|6天后|一天之后|两天之后|三天之后|四天之后|五天之后|六天之后|1天之后|2天之后|3天之后|4天之后|5天之后|6天之后|一天以后|两天以后|三天以后|四天以后|五天以后|六天以后|1天以后|2天以后|3天以后|4天以后|5天以后|6天以后)的天气情况")
         #TIME_PATTERN = re.compile(r"为你查询城市：\w+([^天]+)天的天气情况")
 
         match = CITY_PATTERN.search(response_text)
@@ -42,28 +45,27 @@ class WeatherPlugin(BasePlugin):
 
 
 
-            elif match.group(2) == "明天" or match.group(2) == "1天后" or match.group(2) == "一天后":
+            elif match.group(2) == "明天" or match.group(2) == "1天后" or match.group(2) == "一天后"or match.group(2) == "1天之后" or match.group(2) == "一天之后"or match.group(2) == "1天以后" or match.group(2) == "一天以后":
                 W_time = 1
 
 
-            elif match.group(2) == "后天" or match.group(2) == "2天后" or match.group(2) == "二天后" or match.group(
-                    2) == "两天后":
+            elif match.group(2) == "后天" or match.group(2) == "2天后" or match.group(2) == "两天后"or match.group(2) == "2天之后" or match.group(2) == "两天之后"or match.group(2) == "2天以后" or match.group(2) == "两天以后":
                 W_time = 2
 
 
-            elif match.group(2) == "大后天" or match.group(2) == "3天后" or match.group(2) == "三天后":
+            elif match.group(2) == "大后天" or match.group(2) == "3天后" or match.group(2) == "三天后"or match.group(2) == "3天之后" or match.group(2) == "三天之后"or match.group(2) == "3天以后" or match.group(2) == "三天以后":
                 W_time = 3
 
 
-            elif match.group(2) == "大大后天" or match.group(2) == "4天后" or match.group(2) == "四天后":
+            elif match.group(2) == "大大后天" or match.group(2) == "4天后" or match.group(2) == "四天后"or match.group(2) == "4天之后" or match.group(2) == "四天之后"or match.group(2) == "4天以后" or match.group(2) == "四天以后":
                 W_time = 4
 
 
-            elif match.group(2) == "大大大后天" or match.group(2) == "5天后" or match.group(2) == "五天后":
+            elif match.group(2) == "大大大后天" or match.group(2) == "5天后" or match.group(2) == "五天后"or match.group(2) == "5天之后" or match.group(2) == "五天之后"or match.group(2) == "5天以后" or match.group(2) == "五天以后":
                 W_time = 5
 
 
-            elif match.group(2) == "大大大大后天" or match.group(2) == "6天后" or match.group(2) == "六天后":
+            elif match.group(2) == "大大大大后天" or match.group(2) == "6天后" or match.group(2) == "六天后"or match.group(2) == "6天之后" or match.group(2) == "六天之后"or match.group(2) == "6天以后" or match.group(2) == "六天以后":
                 W_time = 6
 
             else:
